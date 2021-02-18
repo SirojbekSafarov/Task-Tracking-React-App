8232e7ee1bef8f53cf2a19c8c8e50254d2ea7f2e
Sirojbek <sirojbeksafarov@gmail.com>
2021-02-18 13:11:30 +0900

    My first commit

---
+++ src/App.js
@@ -0,0 +1,108 @@
+
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import {useState, useEffect} from 'react'
+import AddTask from './components/AddTask'
+import Footer from './components/Footer'
+import { BrowserRouter as Rauter, Route} from 'react-router-dom'
+import About from './components/About'
+
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+
+    getTasks()
+  }, [])
+
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
+
+  // Fetch Task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+
+    return data
+  }
+
+  // Add Task
+  const addTask = async (task) =>{
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(task),
+    })
+    
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+    
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newTask = { id, ...task }
+
+    // setTasks([...tasks, newTask])
+  }
+  // Delete Task
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`,{
+      method: 'DELETE'
+    })
+
+    setTasks(tasks.filter((task)=>task.id !== id))
+  }
+
+  // Toggle remainder
+  const toggle = async (id) => {
+    const taskToToggle = await fetchTask(id) 
+    const updTask = {...taskToToggle, 
+      remainder: !taskToToggle.remainder
+    }
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`,{
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updTask)
+    })
+
+    const data = await res.json()
+    setTasks(
+      tasks.map((task) =>task.id === id ? {
+        ...task, remainder: data.remainder} : task)
+    )
+  } 
+
+  return (
+    <Rauter>
+      <div className="container">
+        <Header onAdd={()=> setShowAddTask(!showAddTask)} 
+        showAdd={showAddTask}/>
+        <Route path='/' exact render={(props) => (
+          <>
+            {showAddTask && <AddTask onAdd={addTask}/>}
+            {tasks.length > 0 ? <Tasks tasks={tasks} 
+            onDelete={deleteTask} onToggle={toggle}/> : 'No task'}
+          </>
+        )}/>
+        <Route path='/about' component={About} />
+        <Footer />
+      </div>
+    </Rauter>
+  );
+}
+
+export default App;
